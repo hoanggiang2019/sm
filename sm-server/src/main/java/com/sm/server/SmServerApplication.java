fee31987f760d209797c4fc41176f872a2f9ad8d
Giang <hbgiang2019@gmail.com>
2023-11-16 13:51:20 +0700

    first commit

---
+++ sm-server/src/main/java/com/sm/server/SmServerApplication.java
@@ -0,0 +1,13 @@
+package com.sm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmServerApplication.class, args);
+    }
+
+}
