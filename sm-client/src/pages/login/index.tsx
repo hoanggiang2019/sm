ff94b1596dbd528e017e162154973ebca18d5cb7
Giang <hbgiang2019@gmail.com>
2023-11-16 23:48:58 +0700

    first commit

---
+++ sm-client/src/pages/login/index.tsx
@@ -0,0 +1,64 @@
+import Head from "next/head";
+
+import {Button, Card, Checkbox, Input, Typography,} from "@material-tailwind/react";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Next.js</title>
+            </Head>
+            <div className={"flex justify-center items-center h-screen"}>
+                <Card color="transparent" shadow={false}>
+                    <Typography variant="h4" color="blue-gray" className={"text-center"}>
+                        Login Account
+                    </Typography>
+                    <Typography color="gray" className="mt-1 font-normal text-center">
+                        Welcome to back!
+                    </Typography>
+                    <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                        <div className="mb-1 flex flex-col gap-6">
+                            <Typography variant="h6" color="blue-gray" className="-mb-3">
+                                Username
+                            </Typography>
+                            <Input
+                                size="lg"
+                                placeholder="username"
+                                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                                labelProps={{
+                                    className: "before:content-none after:content-none",
+                                }}
+                            />
+                            <Typography variant="h6" color="blue-gray" className="-mb-3">
+                                Password
+                            </Typography>
+                            <Input
+                                type="password"
+                                size="lg"
+                                placeholder="********"
+                                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                                labelProps={{
+                                    className: "before:content-none after:content-none",
+                                }}
+                            />
+                        </div>
+                        <Checkbox
+                            label={
+                                <Typography variant="small" color="gray" className="flex items-center font-normal">
+                                    I agree the
+                                    <a href="#" className="font-medium transition-colors hover:text-gray-900">
+                                        &nbsp;Terms and Conditions
+                                    </a>
+                                </Typography>
+                            }
+                            containerProps={{className: "-ml-2.5"}}
+                        />
+                        <Button className="mt-6" fullWidth>
+                            sign up
+                        </Button>
+                    </form>
+                </Card>
+            </div>
+        </>
+    );
+}
